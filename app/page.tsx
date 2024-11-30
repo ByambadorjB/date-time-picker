90cf5557aeb6355f78038080d4a4fe4bc9de76e1
ByambadorjB <btbyambadorj@gmail.com>
2024-11-30 12:33:15 +1100

    added autocomplete search address and it returns actual address and centered marker is pinned and it is mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import DateTimePickerDialog from "@/components/time-picker/date-time-picker-dialog";
 import MobileNav from "@/components/ui/mobile-nav";
 import Sidebar from "@/components/ui/sidebar";
-// import DateTimePickerForm from "@/components/time-picker/date-time-picker-form";
-// import { DatePicker } from "@/components/ui/date-picker";
-// import Image from "next/image";
+import Map from "@/components/map";
 
 export default function Home() {
+  const latlong = {
+    lat: -33.88204726572486, // Example latitude
+    lng: 151.20646287457745 // Example longitude
+  };
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       {/* Sidebar Section */}
@@ -16,12 +19,16 @@ export default function Home() {
 
       {/* Main Content Section */}
       <main className="relative p-5">
-        <h1 className="absolute top-5 left-1/2 transform -translate-x-1/2 text-xl font-bold">
-          Welcome to Lunch buddy, <br />
-          Connecting friends, one lunch at a time!
-        </h1>
-        
-        <div className="flex items-center justify-center h-full">
+        <div>
+          <h1 className="absolute top-5 left-1/2 transform -translate-x-1/2 text-xl font-bold">
+            Welcome to Lunch buddy, <br />
+          </h1>
+
+        </div>
+        <br />
+        <br />
+        <div className="">
+          <Map {...latlong} />
           <DateTimePickerDialog />
         </div>
         
---
+++ components/map.tsx
@@ -0,0 +1,117 @@
+'use client'
+
+import React from "react"
+import { LatLong } from '@/types';
+import { useState } from "react";
+import { useJsApiLoader } from "@react-google-maps/api";
+import { Library } from "@googlemaps/js-api-loader";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { Input } from "./ui/input";
+import { Label } from "@radix-ui/react-label";
+
+const libs: Library[] = ["core", "maps", "places", "marker" ]
+
+
+function Map(latlong: LatLong) {
+
+    const [map, setMap] = useState<google.maps.Map | null>(null)
+    const [autoComplete, setAutoComplete] = useState<google.maps.places.Autocomplete | null> (null)
+
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string, 
+        libraries: libs
+    })
+
+    const mapRef = useRef<HTMLDivElement>(null)
+    const placeAutoCompleteRef = useRef<HTMLInputElement>(null)
+
+    const [selectedPlace, setSelectedPlace] =useState<string | null>(null)
+
+    useEffect(() => {
+        if(isLoaded){
+            console.log("Google Maps API loaded, initializing map...");
+            const mapOptions = {
+                center: {
+                    // lat: latlong.coordinates[0],
+                    // lng: latlong.coordinates[1]
+                    lat: latlong.lat,
+                    lng: latlong.lng
+                }, 
+                zoom: 17,
+                mapId: 'Map-1234'
+            }
+
+            // Setup the map
+            const gMap = new google.maps.Map(mapRef.current as HTMLDivElement, mapOptions)
+            
+            // limit the place bounds
+            const sydneyBounds = new google.maps.LatLngBounds(
+                new google.maps.LatLng({ lat: -33.948889583475015, lng: 151.00057495422325}),
+                new google.maps.LatLng({ lat: -33.747252372905486, lng: 151.27928808261808})
+                
+            )
+            // setup autocomplete
+            const gAutoComplete = new google.maps.places.Autocomplete(placeAutoCompleteRef.current as HTMLInputElement, {
+                bounds: sydneyBounds,
+                fields: ['formatted_address', 'geometry', 'name',],
+                componentRestrictions: {
+                    country: ['au']
+                }
+            })
+            setAutoComplete(gAutoComplete)
+            setMap(gMap);
+        }
+    }, [isLoaded, latlong])
+
+    useEffect(() => {
+        if(autoComplete){
+            autoComplete.addListener('place_changed', () => {
+                const place = autoComplete.getPlace()
+                const field = autoComplete.getFields()
+                const bounds = autoComplete.getBounds()
+                console.log('Place' + {place})
+                console.log(place)
+                console.log('Fileds' + {field})
+                console.log(field)
+                console.log('Place' + {bounds})
+                console.log(bounds)
+                setSelectedPlace(place.formatted_address as string)
+                const position = place.geometry?.location
+
+                if(position){
+                    // place a marker
+                    setMarker(position, place.name!)
+                }
+            })
+        }
+    }, [autoComplete])
+
+    function setMarker(location: google.maps.LatLng, name: string){
+
+        if(!map) return
+
+        map.setCenter(location)
+        const marker = new google.maps.marker.AdvancedMarkerElement({
+            map: map,
+            position: location, 
+            title: "Marker"
+        })
+    }
+
+    return (
+        <div className="flex flex-col space-y-4">
+            <Input ref={placeAutoCompleteRef}/>
+            <Label>
+                {selectedPlace}
+            </Label>
+            {isLoaded ?
+                <div style={{height: '400px'}} ref = {mapRef} />
+                : <p>Loading ...</p>
+                
+            }
+        </div>
+    )
+}
+
+export default Map
