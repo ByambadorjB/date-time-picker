f541b658afcd0fec5ea407a43f030428af43b385
ByambadorjB <btbyambadorj@gmail.com>
2024-12-15 20:52:42 +1100

    Mobile navigation and side bar are fixed on phone view

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+'use client'
 import DateTimePickerDialog from "@/components/time-picker/date-time-picker-dialog";
 import MobileNav from "@/components/ui/mobile-nav";
 import Sidebar from "@/components/ui/sidebar";
 import Map from "@/components/map";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+  
   const latlong = {
     lat: -33.88204726572486, // Example latitude
     lng: 151.20646287457745 // Example longitude
@@ -12,10 +17,17 @@ export default function Home() {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       {/* Sidebar Section */}
-      <aside className="bg-gray-300 h-screen">
+      <aside className={`bg-gray-300 h-screen absolute md:relative transform transition-transform duration-300 ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0`}>
         <Sidebar />
-        <MobileNav />
+        {/* <MobileNav /> */}
       </aside>
+      {/* Mobile Navigation */}
+      <nav className="md:hidden">
+        {/* Mobile Navigation (appears alongside the hamburger menu) */}
+        <MobileNav />
+      </nav>
 
       {/* Main Content Section */}
       <main className="relative p-5">
